e65114a4240863299d2b69c820bd17afbfaa6b58
Sabrina Ly <ly.domenica@gmail.com>
2026-08-30 19:46:54 -0500

    comenzando con las entradas del diario

--- p-diary/Source.cpp
+++ p-diary/Source.cpp
@@ -2,6 +2,7 @@
 #include <conio.h>
 #include <cstring>
 #include <cstdlib> 
+#include <ctime>
 
 using namespace std;
 using namespace System;
@@ -21,7 +22,7 @@ entrada* diario = nullptr;
 int totalentradas = 0;
 int siguienteid = 1; 
 
-
+//obtener fecha? 
 void obtenerfecha(char* fecha)
 {
 	time_t t;
@@ -29,4 +30,49 @@ void obtenerfecha(char* fecha)
 	time(&t);
 	info = localtime(&t);
 	strftime(fecha, 30, "%d/%m/%Y  %H:%M", info);
+}
+
+//contar palabras
+int contarpalabras(char* texto)
+{
+	int contador = 0;
+	int enpalabra = 0;
+
+	for (int i = 0; texto[i] != '\0'; i++)
+	{
+		if (texto[i] != ' ' && texto[i] != '\n' && texto[i] != '\t')
+		{
+			if (!enpalabra)
+			{
+				contador++;
+				enpalabra = true;
+			}
+
+		}
+		else
+		{
+			enpalabra = false;
+		}
+	}
+}
+
+void agregarentrada()
+{
+	diario = (entrada*)realloc(diario, (totalentradas + 1) * sizeof(entrada));
+
+	cout << "NUEVA ENTRADA";
+	cout << "TITULO: ";
+	cin.getline(diario[totalentradas].titulo, 100);
+	if (strlen(diario[totalentradas].titulo) == 0)
+	{
+		cin.getline(diario[totalentradas].titulo, 100);
+	}
+
+	cout << "CONTENIDO: ";
+	cin.getline(diario[totalentradas].contenido, 1000);
+
+	cout << "EMOCION (feliz, triste, enojado, cansado, enamorado): ";
+	cin.getline(diario[totalentradas].emocion, 20);
+
+
 }
